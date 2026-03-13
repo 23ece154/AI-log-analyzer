a6b244d86a1ebc789457bfb40c442694764a3d69
23ece154 <srikarthika.l2023ece@sece.ac.in>
2026-03-13 15:53:02 +0530

    changed server.js code

--- backend/server.js
+++ backend/server.js
@@ -8,25 +8,7 @@ const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://ai-log-analyzer-one.vercel.app",
-  "https://ai-log-analyzer-1oc2jqvj0-sri-karthikas-projects.vercel.app"
-];
-
-app.use(cors({
-  origin: function(origin, callback) {
-    if (!origin) return callback(null, true); // allow Postman, etc.
-    if (allowedOrigins.indexOf(origin) === -1) {
-      return callback(new Error("CORS not allowed"), false);
-    }
-    return callback(null, true);
-  },
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-}));
-
-
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/logs", logRoutes);
@@ -43,4 +25,4 @@ mongoose.connect(process.env.MONGO_URI)
 
 app.listen(5000, () => {
   console.log(`Server running on port 5000`)
-});
+})
